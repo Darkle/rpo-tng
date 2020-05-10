ffa9441697fff4ded1cbab3d63a4d7d2c27d5113
Coop <coop.coding@gmail.com>
2020-05-10 19:49:09 +1000

    Set up Typescript build.

--- src/ts/index.js
+++ src/ts/index.js
@@ -1,2 +1,5 @@
-"use strict";
+import { html } from "https://unpkg.com/lit-html@1.2.1?module";
 html `<my-element></my-element>`;
+html `<div>`;
+html `<input @iinput="${console.log}" />`;
+console.log("bar");
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,7 @@
-import {html} from '../../web_modules/lit-html.js'
+import {html} from 'lit-html'
 
 html`<my-element></my-element>`
 html`<div>`
 html`<input @iinput="${console.log}" />`
 
+console.log(process.env.ISDEV)
