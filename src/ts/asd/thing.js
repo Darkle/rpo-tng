25cc632e0facba0322a7a5fae32583f275cc0d8e
Coop <coop.coding@gmail.com>
2020-05-19 12:55:25 +1000

    Updated devNotes.md

--- src/ts/asd/thing.js
+++ src/ts/asd/thing.js
@@ -1,5 +1,10 @@
-import { html } from 'lit-html';
-const divver = () => html `<div>`;
-html `<input @iinput="${console.log}" />`;
-function foo() { }
+import { html } from "/web_modules/lit-html.js"; // html`<my-element></my-element>`
+
+const divver = () => html`<div>`;
+
+html`<input @iinput="${console.log}" />`;
+
+function foo() {}
+
 export { foo };
+//# sourceMappingURL=thing.js.map
